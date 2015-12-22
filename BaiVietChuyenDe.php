e5b246d87c89f9c082b80859a50748cc854fc28d
lmtca07tha <lmtca07tha@gmail.com>
2015-12-22 16:33:57 +0700

    tag html
    html test

--- BaiVietChuyenDe.php
+++ BaiVietChuyenDe.php
@@ -6,10 +6,12 @@ while($row_baiviet = mysql_fetch_array($baiviet)){
 ?>
 <!-- Mot bai viet 1-->
 <div id="baiviet">
-	<h4><a href="index.php?p=chitietbaiviet&idBaiViet=<?php echo $row_baiviet['idBaiViet']?>"><?php echo $row_baiviet['TieuDeBaiViet']?></a></h4>
-    <img id="hinhbaiviet" class="images_news" src="./hinh/<?php echo $row_baiviet['urlHinh']?>" align="left"/>
-    </br>
-    <div id="tomtatbaiviet"><?php echo $row_baiviet['TomTat']?></div>
+	<article><!--thẻ article định nghĩa một bài viết-->
+		<h1><a href="index.php?p=chitietbaiviet&idBaiViet=<?php echo $row_baiviet['idBaiViet']?>"><?php echo $row_baiviet['TieuDeBaiViet']?></a></h1>
+		<img id="hinhbaiviet" class="images_news" src="./hinh/<?php echo $row_baiviet['urlHinh']?>" align="left"/>
+		</br>
+		<p id="tomtatbaiviet"><?php echo $row_baiviet['TomTat']?></p>
+	</article>
     <div class="clear"></div>
 </div>
 <?php
